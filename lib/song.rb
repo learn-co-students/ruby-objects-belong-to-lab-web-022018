e579342926cb49b8abd7211b0f84e9e97fec1e55
github username <github email address>
2018-02-24 17:19:04 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+
+
+class Song
+
+  def title=(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def artist
+    @artist
+  end
+end
